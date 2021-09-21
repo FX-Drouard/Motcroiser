62172aee87f82764225d56f5cd2974a8046b2a6a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-21 17:43:09 +0200

    exo 2 bug

--- src/pobj/motx/tme1/Grille.java
+++ src/pobj/motx/tme1/Grille.java
@@ -1,5 +1,41 @@
 package pobj.motx.tme1;
 
 public class Grille {
+private Case la[][];
+
+	public Grille(int hauteur, int largeur) {
+		this.la= new Case[hauteur][largeur];
+		for (int i=0; i<hauteur;i++) {
+			for (int j=0; j<largeur;j++) {
+				this.la[i][j]=new Case(hauteur,largeur,' ');
+			}
+		}
+	}
+
+	public Case getCase(int x, int y) {
+		return la[x][y];
+	}
 	
+	public String toString() {
+		return GrilleLoader.serialize(this, false);
+	}
+	
+	public int nbLig() {
+		return this.la.length;
+	}
+	
+	public int nbCol() {
+		return this.la[0].length;
+	}
+	
+	public Grille copy() {
+		Grille temp=new Grille(this.nbLig(),this.nbCol());
+		for (int i=0;i<this.nbLig();i++) {
+			for (int j=0;j<this.nbCol();j++) {
+				temp.la[i][j]=new Case(this.la[i][j].getLig(),this.la[i][j].getCol(),this.la[i][j].getChar());
+			}
+		}
+		if (temp.la[0][0].getChar()==' ') {System.out.println("VEJHDGEJHGHEGFJHDED");}
+		return temp;
+	}
 }

2cb8b69217b56e6d435690128a2e0e7f5e41dbe8
Fristorm-Palifen <fxhbbd@gmail.com>
2021-09-23 17:32:09 +0200

    Oublie d'un fichier -_-

---
+++ src/pobj/motx/tme1/Emplacement.java
@@ -0,0 +1,54 @@
+package pobj.motx.tme1;
+import java.util.ArrayList;
+
+/**
+* Classe permettant de gérer les mots dont les lettres sont contenues dans des Cases!
+*/ 
+public class Emplacement {
+	/** attribut permettant de stocker un mot à partir des Cases! **/
+	private ArrayList<Case> cases;
+	
+	/**
+	* Construit un Emplacement contenant les Cases qui contiennent les lettres d'un mot 
+	*/ 
+	public Emplacement() {
+		this.cases = new ArrayList<Case>();	
+	}
+	
+	/**
+	* Ajoute à l'Emplacement une Case
+	* @param e La Case en question
+	*/
+	public void addCase(Case e) {
+		this.cases.add(e);
+	}
+	
+	/**
+	* Permet d'afficher la taille de l'Emplacement
+	* @return la taille
+	*/
+	public int size() {
+		return this.cases.size();
+	}
+	
+	/**
+	* Donne accès à la Case n° i de l'Emplacement
+	* @param i Le numéro de la Case en question
+	* @return la Case
+	*/
+	 public Case getCase(int i) {
+		 return this.cases.get(i);
+	 }
+	 
+	 /**
+		* Ecrit le contenue de l'Emplacement
+		* @return Le mot généré
+		*/
+	 public String toString() {
+		 String res="";
+		 for (int i = 0; i<this.cases.size(); i++) {
+			 res+=this.cases.get(i).getChar();
+		 }
+		 return "Le mot est: "+res;
+	 }
+}

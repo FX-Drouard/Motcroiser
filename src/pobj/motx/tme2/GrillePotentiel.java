7c23615b1658adcd1a44eb8287373e2ed93dbfd7
Sorabush <alexandreyop@hotmail.fr>
2021-10-03 08:31:08 +0200

    finition de GillePotentiel

--- src/pobj/motx/tme2/GrillePotentiel.java
+++ src/pobj/motx/tme2/GrillePotentiel.java
@@ -5,16 +5,20 @@ import java.util.List;
 import pobj.motx.tme1.*;
 
 public class GrillePotentiel {
-	//private GrillePlaces grille;
+	private GrillePlaces grille;
 	private Dictionnaire dikushioneiru;
 	private List<Dictionnaire> motsPot;
 	
-	public GrillePotentiel(/*GrillePlaces grille,*/Dictionnaire dicoComplet){
-		//this.grille=grille;
+	public GrillePotentiel(GrillePlaces grille,Dictionnaire dicoComplet){
+		this.grille=grille;
 		this.dikushioneiru=dicoComplet;
-		//Il faut limiter ajouter dans motsPot le dictionnaire filtré pour chaque emplacement
+		//CHAQUE dictionnaire sera limité dans l'ordre des emplacements
 		List<Dictionnaire> motsPot= new ArrayList<>();
-		
+		for(int i=0;i<this.grille.getPlaces().size();i++){
+			Dictionnaire copium= dicoComplet.copy();
+			copium.filtreLongueur(this.grille.getPlaces().get(i).size());
+			motsPot.add(copium);
+		}
 	}
 	
 	public boolean isDead() {

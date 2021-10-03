fabb4946eccf3b5671bf3010977d752de8495448
BuildTools <unconfigured@null.spigotmc.org>
2021-10-03 15:34:55 +0200

    correction classe grilleplaces

--- src/pobj/motx/tme2/GrillePlaces.java
+++ src/pobj/motx/tme2/GrillePlaces.java
@@ -5,57 +5,71 @@ import java.util.ArrayList;
 
 public class GrillePlaces {
 private List<Emplacement> places;
-private int hor;
+private int hor =0;
 
 public GrillePlaces (Grille grille) {
-	this.places = new ArrayList<>();
-	int x = grille.nbLig();
-	int y = grille.nbCol();
-	this.hor=0;
-	Emplacement temp = new Emplacement();
-	for (int i =0; i<x;i++) {
-		for (int j=0; j<y;j++) {
-			if ((grille.getCase(i, j).getChar()=='*')||(temp.size()>=2)) {
-				places.add(temp);
-				temp=new Emplacement();
-			}else if((grille.getCase(i, j).getChar()=='*')||(temp.size()<2)) {
-				temp=new Emplacement();
-			}else {
-				if (grille.getCase(i, j).getChar()=='*') {continue;}else {
-				temp.addCase(grille.getCase(i, j));
-				//ioo
-			}
-				}
-		}
-		places.add(temp);
-		temp=new Emplacement();
-	}
-	hor=places.size();
-	System.out.println("la= "+hor);
-	for (int j =0; j<y;j++) {
-		for (int i=0; i<x;i++) {
-			if (grille.getCase(i, j).getChar()==' ') {
-				places.add(temp);
-				temp=new Emplacement();
-			}else {
-				temp.addCase(grille.getCase(i, j));
-			}
-		}
-		places.add(temp);
-		temp=new Emplacement();
+	//on commence par l'Horizontal
+	for (int i = 0; i<grille.nbLig();i++) {
+		this.cherchePlacesLignes(this.getLig(grille,i));
 	}
 	
-	
+	//Vertical
+	for (int j=0; j<grille.nbCol();j++) {
+		this.cherchePlacesColonnes(this.getCol(grille,j));
+	}
 }
+
+private void cherchePlacesLignes(List <Case> cases) {
+	Emplacement e = new Emplacement();
+	int i;
+	for (i =0; i<cases.size();i++) {
+		if (cases.get(i).getChar() != '*') {
+			e.addCase(cases.get(i));
+		}else {
+			if (e.size() >1) {
+				places.add(e);
+				this.hor++;
+			}
+			e = new Emplacement();
+		}
+	}
+	if (e.size() >=2) {
+		places.add(e);
+		e= new Emplacement();
+		hor++;
+	}
+}
+
+private void cherchePlacesColonnes(List <Case> cases) {
+	Emplacement e = new Emplacement();
+	int j;
+	for (j =0; j<cases.size();j++) {
+		if (cases.get(j).getChar() != '*') {
+			e.addCase(cases.get(j));
+		}else {
+			if (e.size() >=2) {
+				places.add(e);
+			}
+			e = new Emplacement();
+		}
+	}
+	if (e.size() >=2) {
+		places.add(e);
+		e= new Emplacement();
+	}
+}
+
 
 public List<Emplacement> getPlaces() {
 	return places;
 }
 
-
-public int getNbHorizontal() {
-	return this.hor;
+public int getnbHorizontal() {
+	return hor;
 }
+
+
+
 
 
 }

7b8461e89bb3a0d436dc32441057af51c8baecc0
BuildTools <unconfigured@null.spigotmc.org>
2021-10-03 17:14:59 +0200

    Fixer et javadoc GrillePlaces

--- src/pobj/motx/tme2/GrillePlaces.java
+++ src/pobj/motx/tme2/GrillePlaces.java
@@ -3,11 +3,21 @@ import java.util.List;
 import pobj.motx.tme1.*;
 import java.util.ArrayList;
 
+/**
+* Classe permettant de gérer une Grille de Case et ses emplacements
+*/ 
 public class GrillePlaces {
+/** attribut permettant de stocker les Emplacement de case **/
 private List<Emplacement> places;
+/** attribut permettant de stocker l'indice du dernier emplacement horizontal! **/
 private int hor =0;
+/** attribut permettant de stocker la grille**/
 private Grille grill;
 
+/**
+* Construit une GrillePlaces une grille, et ses emplacements:
+* @param grille La grille de depart
+*/ 
 public GrillePlaces (Grille grille) {
 	this.grill=grille;
 	//on commence par l'Horizontal
@@ -22,6 +32,10 @@ public GrillePlaces (Grille grille) {
 	}
 }
 
+/**
+* initialise la list des emplacement en ligne:
+* @param x La liste de Case a chercher
+*/ 
 private void cherchePlacesLignes(List <Case> cases) {
 	Emplacement e = new Emplacement();
 	int i;
@@ -43,6 +57,10 @@ private void cherchePlacesLignes(List <Case> cases) {
 	}
 }
 
+/**
+* initialise la list des emplacement en Colonne:
+* @param x La liste de Case a chercher
+*/ 
 private void cherchePlacesColonnes(List <Case> cases) {
 	Emplacement e = new Emplacement();
 	int j;
@@ -62,15 +80,27 @@ private void cherchePlacesColonnes(List <Case> cases) {
 	}
 }
 
-
+/**
+* Permet d'obtenir la list des emplacements
+* @return la liste des emplacements
+*/
 public List<Emplacement> getPlaces() {
 	return places;
 }
-
+/**
+* Permet d'afficher le nombre d'emplacement horizontaux
+* @return le nombre d'emplacement horizontaux
+*/
 public int getNbHorizontal() {
 	return hor;
 }
 
+/**
+* Permet d'obtenir la list des case d'une ligne
+* @param g La grille
+* @param i L'indice de la ligne
+* @return la list des case d'une ligne
+*/
 private List<Case> getLig(Grille g, int i){
 	List<Case> temp = new ArrayList<>();
 	for (int r=0;r<g.nbCol();r++) {
@@ -79,6 +109,12 @@ private List<Case> getLig(Grille g, int i){
 	return temp;
 }
 
+/**
+* Permet d'obtenir la list des case d'une colonne
+* @param g La grille
+* @param j L'indice de la colonne
+* @return la list des case d'une colonne
+*/
 private List<Case> getCol(Grille g, int j){
 	List<Case> temp = new ArrayList<>();
 	for (int r=0;r<g.nbLig();r++) {
@@ -87,6 +123,12 @@ private List<Case> getCol(Grille g, int j){
 	return temp;
 }
 
+/**
+* Permet de modifier une grille avec un mot a rentrer
+* @param m L'indice de l'emplacement
+* @param soluce le mot
+* @return une nouvelle GrillePlaces modifier
+*/
 public GrillePlaces fixer(int m, String soluce) {
 	Grille tmp2=this.grill.copy();
 	GrillePlaces res= new GrillePlaces(tmp2);
@@ -100,5 +142,15 @@ public GrillePlaces fixer(int m, String soluce) {
 	res.getPlaces().set(m, e);
 	return res;
 }
+
+/**
+* Permet d'obtenir la grille
+* @return le nombre de ligne
+*/
+public Grille getGrille() {
+	return grill.copy();
+}
+
+
 
 }
